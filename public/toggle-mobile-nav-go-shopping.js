e28ff86fe04566a29fb82597bc06a60336fe62bd
davisrukitis <davis@rukitis.com>
2025-02-11 14:22:12 +0100

    Delete public directory

--- public/toggle-mobile-nav-go-shopping.js
+++
@@ -1,9 +0,0 @@
-const navGSFooter = document.querySelector(".mobile-nav .go-shopping footer")
-const navGSCenterItems = document.querySelector(".mobile-nav .go-shopping .center-items")
-const footerIcon = document.querySelector(".mobile-nav .go-shopping footer .ico")
-
-navGSFooter.addEventListener("click", () => {
-    navGSCenterItems.classList.toggle("center-items-hidden")
-    navGSFooter.classList.toggle("while-selector-visible")
-    footerIcon.classList.toggle("spin")
-})
