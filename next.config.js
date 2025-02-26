6cb60dc8b0b0b43bfec7976b5d6ba16d52b5243c
davisrukitis <davis@rukitis.com>
2025-02-26 13:31:20 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["rimirigamarathon.com", "cdn.prod.website-files.com"]
-  }
-};
-module.exports = nextConfig;
